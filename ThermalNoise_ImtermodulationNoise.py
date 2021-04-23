9b69863b54e64f4cf057dac48233df5ad41a483a
xjchen2008 <xjchen2008@gmail.com>
2021-04-23 02:42:50 -0400

    Study the convolution of intermodulation noise floor. So put the 20log() away.

--- ThermalNoise_ImtermodulationNoise.py
+++ ThermalNoise_ImtermodulationNoise.py
@@ -1,4 +1,4 @@
-
+# G. T. Zhou and J. S. Kenney, "Predicting spectral regrowth of nonlinear power amplifiers," in IEEE Transactions on Communications, vol. 50, no. 5, pp. 718-722, May 2002, doi: 10.1109/TCOMM.2002.1006553.
 import numpy as np
 import matplotlib.pyplot as plt
 #############
@@ -7,11 +7,11 @@ import matplotlib.pyplot as plt
 c = 3e8
 j = 1j
 fs = 56*2e6  # Sampling freq
-N = 400000  # This also limit the bandwidth. And this is determined by fpga LUT size.
+N = 4000  # This also limit the bandwidth. And this is determined by fpga LUT size.
 T = N/fs  # T=N/fs#Chirp Duration
 t = np.linspace(0, T, N)
-f0 = -28e6  # Start Freq
-f1 = 28e6  # fs/2=1/2*N/T#End freq
+f0 = -10e6  # -28e6 Start Freq
+f1 = 10e6  # 28e6 fs/2=1/2*N/T#End freq
 K = (f1 - f0) / T # chirp rate = BW/Druation
 phi0 = (4999+1) * np.pi / 10000  # Phase
 f = np.linspace(0, fs-1, N)
@@ -42,33 +42,39 @@ y_cx_delay_3 = np.concatenate((x_cx[N-d3-1:-1], x_cx[:N-d3]), axis=0)
 vin1 = 1e-3*y_cx_delay_1 + 1e-7*y_cx_delay_2 + 1e-5*y_cx_delay_3 # With leakage, no intermodulatoin #1*y_cx_delay_1 + 1e-7*y_cx_delay_2 + 1e-5*y_cx_delay_3
 vin2 = 1e-7*y_cx_delay_2 + 1e-5*y_cx_delay_3 # No leakage signal
 vin2 = vin2 + 0.001*np.power(vin2,2)- 0.001*np.power(vin2,3) # Intermodulation of vin2
-vin3 = vin1 + 0.001*np.power(vin1,2)- 0.001*np.power(vin1,3) # Intermodulation of vin2 with leakage
+vin3 = vin1 + 0.001*np.power(vin1,2)- 0.001*np.power(vin1,3) # Intermodulation of vin1 with leakage
 
 # vin2 = y_cx_received # With leakage signal
 
-
-V1 = 20*np.log10(np.fft.fft(vin1))
-V2 = 20*np.log10(np.fft.fft(vin2))
-V3 = 20*np.log10(np.fft.fft(vin3))
+V0 = np.fft.fft(x_cx)#20*np.log10(np.fft.fft(x_cx))
+V1 = np.fft.fft(vin1) #20*np.log10(np.fft.fft(vin1))
+V2 = np.fft.fft(vin2)#20*np.log10(np.fft.fft(vin2))
+V3 = np.fft.fft(vin3)#20*np.log10(np.fft.fft(vin3))
+plt.plot(freq, V0-max(V0))
 plt.figure()
-plt.plot(freq, V1-max(V1), freq, V2-max(V1), freq, V3-max(V1))
+plt.plot(freq/1e6, V1-max(V1),'b', freq/1e6, V2-max(V2),'k', freq/1e6, V3-max(V3),'r')
+#plt.plot(freq/1e6, vin1,'b', freq/1e6, vin2,'k', freq/1e6, vin3,'r')
 plt.title(' Intermodulated Chirp in Frequency Domain')
-plt.xlabel('Frequency [Hz]')
+plt.xlabel('Frequency [MHz]')
 plt.ylabel('Normalized Amplitude [dB]')
 plt.grid()
-
+plt.legend(['No Intermodulation with leakge signal',
+           'Intermodulation without leakage signal',
+           'Intermodulation with leakage signal'])
 ######################################
 # Pulse Compression Stretching Method
 #####################################
 def PulseCompr(rx,tx,win):
     A = np.multiply(rx,win) # Add window here
     B = tx
-    PC = 20*np.log10(abs(np.fft.fft(A*np.conj(B))))
+
+    PC = np.fft.fft(A*np.conj(B))
+    #PC = 20 * np.log10(abs(PC))
     return PC
 
 PC = PulseCompr(vin1,x_cx,np.blackman(N))
 PC_intermod = PulseCompr(vin2,x_cx,np.blackman(N))
-PC_intermod_leakage = PulseCompr(vin3,x_cx,np.blackman(N))
+PC_intermod_leakage = PulseCompr(vin3,vin3,np.blackman(N))
 #f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 #ax1.plot(np.fft.fftshift(PC))
 #ax1.set_xlabel('Number of points')
@@ -78,16 +84,17 @@ PC_intermod_leakage = PulseCompr(vin3,x_cx,np.blackman(N))
 #ax2.set_xlim((-10,500))
 #ax2.set_xlabel('Distance in meter')
 #ax2.set_ylabel('Power in dB')
-plt.close()
+#plt.close()
 plt.figure()
-plt.plot(distance,PC-max(PC), '--', distance,PC_intermod-max(PC), 'k-', distance, PC_intermod_leakage-max(PC), 'r.-')
-#plt.plot(distance,PC-max(PC))
+#plt.plot(distance/1e3,PC-max(PC), '--', distance/1e3,PC_intermod-max(PC), 'k-', distance/1e3, PC_intermod_leakage-max(PC), 'r.-')
+plt.plot(distance/1e3,PC-max(PC), 'b', distance/1e3,PC_intermod-max(PC), 'k', distance/1e3, PC_intermod_leakage-max(PC), 'r')
+plt.grid()
 plt.legend(['No Intermodulation with leakge signal',
            'Intermodulation without leakage signal',
            'Intermodulation with leakage signal'])
 
 plt.title('Pulse Compression')
-plt.xlim((-10,500))
-plt.xlabel('Distance in meter')
-plt.ylabel('Normalized Power in dB')
+#plt.xlim((-10,500))
+plt.xlabel('Distance km')
+plt.ylabel('Normalized Power dB')
 plt.show()
