8d5fabc96382e4acab877230d214e085eabf9bb4
xjchen2008 <xjchen2008@gmail.com>
2021-05-01 21:54:15 -0400

    Used Nuttall window. Added three signal as rx signal.

--- Part9_SNR_tx.py
+++ Part9_SNR_tx.py
@@ -11,24 +11,27 @@ import functions
 from functions import plot_freq_db as plot_freq
 from functions import PulseCompr, normalize
 from numpy.fft import fftshift
+from scipy import signal
+
 
 freq = (coe.freq/1e6)
 distance = (coe.distance)
 N = coe.N
 mean = 0
-std = 0.01
+std = 0.001
 noise1 = np.random.normal(mean, std, size=N)
 delay = 1000
-win = np.blackman(N)
+win = signal.nuttall(N) #np.blackman(N)
 win1 = np.hamming(N)
 win2 = np.hanning(N)
-x = coe.y_cx + noise1
-x_delay = coe.y_cx_0_delay
+x = coe.y_cx #+ noise1
+x_delay = np.roll(x, delay)
 x_win = np.multiply(x, win)
 x_win1 = np.multiply(x, win1)
 x_win2 = np.multiply(x, win2)
-x_win_delay = np.roll(x_win, -delay)
-
+x_win_delay = np.roll(x_win, delay)
+x_win_delay2 = np.roll(x_win, delay+100)
+x_win_twosig = x_win_delay + np.roll(x_win, 10)+ x_win_delay2
 plot_freq(freq, x, 'b')
 plot_freq(freq, x_win,'k')
 plot_freq(freq, x_win1,'m')
@@ -38,17 +41,28 @@ plt.legend(['No window', 'Blackman window', 'Hamming window', 'Hanning window'])
 plt.xlabel('Frequency [MHz]')
 plt.ylabel('Amplitude [dB]')
 # pulse compression
-pc = PulseCompr(rx=x_delay, tx = x, win = win1)
-pc_win = PulseCompr(rx=x_win_delay,tx= x_win, win =win1)
-pc_win1 = PulseCompr(rx=x_win_delay,tx= x_win1, win =win1)
-pc_win2 = PulseCompr(rx=x_win_delay, tx=x_win2, win =win1)
+'''pc = PulseCompr(rx=x_delay, tx = x, win = win2)
+pc_win = PulseCompr(rx=x_win_delay,tx= x_win, win =win2)
+pc_win1 = PulseCompr(rx=x_win_delay,tx= x_win1, win =win2)
+pc_win2 = PulseCompr(rx=x_win_delay, tx=x_win2, win =win2)
+'''
+#
+plt.close()
+sig=x_win_delay
+plt.plot(sig)
+pc = PulseCompr(rx=x_win_twosig, tx = x_win, win = win2)
+#pc = PulseCompr(rx=x_win_delay, tx = x_win_delay, win = win2)
+#pc_win = PulseCompr(rx=x_win_delay,tx= x_win_delay, win =win2)
+#pc_win1 = PulseCompr(rx=x_win_delay,tx= x_win_delay, win =win2)
+#pc_win2 = PulseCompr(rx=x_win_delay, tx=x_win_delay, win =win2)
 
 plt.figure()
-plt.plot(fftshift(freq), fftshift(pc), 'b')
-plt.plot(fftshift(freq), fftshift(pc_win),'k')
-plt.plot(fftshift(freq), fftshift(pc_win1),'m')
-plt.plot(fftshift(freq), fftshift(pc_win2),'g')
-plt.xlabel('Frequency [MHz]')
+plt.plot(fftshift(distance), fftshift(pc), 'b')
+#plt.plot(fftshift(distance), fftshift(pc_win),'k')
+#plt.plot(fftshift(distance), fftshift(pc_win1),'m')
+#plt.plot(fftshift(distance), fftshift(pc_win2),'g')
+#plt.xlabel('Frequency [MHz]')
+plt.xlabel('distance [m]')
 plt.ylabel('Amplitude [dB]')
 plt.grid()
 plt.title('Pulse Compression')
