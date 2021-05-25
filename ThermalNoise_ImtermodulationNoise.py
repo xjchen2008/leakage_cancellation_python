2f8b165324ae7ee38d387c7c653433afa51bd71a
xjchen2008 <xjchen2008@gmail.com>
2021-05-25 12:03:12 -0400

    1. Using real valued data. Delay a real valude signal, then do the Hilbert transform. 2. Put distance freq plot function locally.

--- ThermalNoise_ImtermodulationNoise.py
+++ ThermalNoise_ImtermodulationNoise.py
@@ -3,31 +3,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.fft import fftshift
 import functions as fn
+from scipy import signal
 
-N = 4000
+
+N = 499999
 win= 1 #np.blackman(N)
-coe = fn.Coe(fc=11e6, bw=20e6, fs=250e6, N=4000) # play with the params here!
+coe = fn.Coe(fc=50e6, bw=20e6, fs=250e6, N=N) # Call the class. play with the params here!
 freq= coe.freq
 distance = coe.distance
 x_cx = coe.y_cx
+x = coe.y_cx.real
+#x_cx = signal.hilbert(coe.y_cx.real, axis = 0)
 #x_cx = x_cx * coe.y_cx_sine2
-x_cx = np.around(np.power(2,15) * x_cx) # simulate the quantization noise in FPGA fixed point
+#x_cx = np.around(np.power(2,15) * x_cx) # simulate the quantization noise in FPGA fixed point
 
 ####################################################
 d1 = 10 #9 # What happened to pulse compression if change the delay from 10 to 9. The phase discontinuity will smear the energy to other frequency. https://www.keysight.com/us/en/assets/7018-06760/application-notes/5952-8898.pdf?success=true
 d2 = 1000
 d3 = 300
-y_cx_delay_1 = np.concatenate((x_cx[N-d1-1:-1], x_cx[:N-d1]), axis=0) # What happened to pulse compression if change the delay from 10 to 9. The phase discontinuity will smear the energy to other frequency.
-y_cx_delay_2 = np.concatenate((x_cx[N-d2-1:-1], x_cx[:N-d2]), axis=0)
-y_cx_delay_3 = np.concatenate((x_cx[N-d3-1:-1], x_cx[:N-d3]), axis=0)
-vin1 = 1e-4*y_cx_delay_1 + 1e-7*y_cx_delay_2 + 1e-5*y_cx_delay_3 # With leakage, no intermodulatoin #1*y_cx_delay_1 + 1e-7*y_cx_delay_2 + 1e-5*y_cx_delay_3
-vin2 = 1e-7*y_cx_delay_2 + 1e-5*y_cx_delay_3 # No leakage signal
+y_delay_1 = np.concatenate((x[N-d1-1:-1], x[:N-d1]), axis=0) # What happened to pulse compression if change the delay from 10 to 9. The phase discontinuity will smear the energy to other frequency.
+y_delay_2 = np.concatenate((x[N-d2-1:-1], x[:N-d2]), axis=0)
+y_delay_3 = np.concatenate((x[N-d3-1:-1], x[:N-d3]), axis=0)
+vin1 = 1e-4*y_delay_1 + 1e-7*y_delay_2 + 1e-5*y_delay_3 # With leakage, no intermodulatoin #1*y_cx_delay_1 + 1e-7*y_cx_delay_2 + 1e-5*y_cx_delay_3
+vin2 = 1e-7*y_delay_2 + 1e-5*y_delay_3 # No leakage signal
 vin2 = vin2 + 0.001*np.power(vin2,2)- 0.001*np.power(vin2,3) # Intermodulation of vin2
-vin3 = vin1 + 0.001*np.power(vin1,2)- 0.001*np.power(vin1,3) + 0.001*np.power(vin1,5) + 0.001*np.power(vin1,7)# Intermodulation of vin1 with leakage
-
+vin3 = vin1 + 0.001*np.power(vin1,2)- 0.001*np.power(vin1,3) # + 0.001*np.power(vin1,5) + 0.001*np.power(vin1,7)# Intermodulation of vin1 with leakage
+# first delay, then recorded in Oscilloscope and go to complex signals
+vin1 = signal.hilbert(vin1)
+vin2 = signal.hilbert(vin2)
+vin3 = signal.hilbert(vin3)
 # vin2 = y_cx_received # With leakage signal
 
-V0 = 20*np.log10(np.fft.fft(x_cx)) #np.fft.fft(x_cx)#20*np.log10(np.fft.fft(x_cx))
+V0 = 20*np.log10(np.fft.fft(x)) #np.fft.fft(x_cx)#20*np.log10(np.fft.fft(x_cx))
 V1 = 20*np.log10(np.fft.fft(vin1)) #20*np.log10(np.fft.fft(vin1))
 V2 = 20*np.log10(np.fft.fft(vin2)) #np.fft.fft(vin2)#20*np.log10(np.fft.fft(vin2))
 V3 = 20*np.log10(np.fft.fft(vin3)) #np.fft.fft(vin3)#20*np.log10(np.fft.fft(vin3))
@@ -35,7 +42,7 @@ plt.plot(fftshift(freq)/1e6, fftshift(V0-max(V0)))
 plt.grid()
 plt.title('Original Signal')
 plt.figure()
-plt.plot(fftshift(freq)/1e6, fftshift(V1-max(V1)),'b', fftshift(freq)/1e6, fftshift(V2-max(V2)),'k', fftshift(freq)/1e6, fftshift(V3-max(V3)),'r')
+plt.plot(fftshift(freq)/1e6, fftshift(V1-max(V1)),'b*', fftshift(freq)/1e6, fftshift(V2-max(V2)),'k', fftshift(freq)/1e6, fftshift(V3-max(V3)),'r')
 #plt.plot(freq/1e6, vin1,'b', freq/1e6, vin2,'k', freq/1e6, vin3,'r')
 plt.title(' Intermodulated Chirp in Frequency Domain')
 plt.xlabel('Frequency [MHz]')
@@ -63,11 +70,27 @@ PC_intermod_leakage = fn.PulseCompr(vin3,x_cx,np.blackman(N))
 #ax2.set_ylabel('Power in dB')
 #plt.close()
 
+
+
+def distance2freq(distance):
+    # https://matplotlib.org/stable/gallery/subplots_axes_and_figures/secondary_axis.html
+    k = coe.k
+    c = 3e8
+    freq = distance / c * k * 2.0
+    return freq / 1e6  # MHz
+
+
+def freq2distance(freq):
+    k = coe.k
+    c = 3e8
+    distance = c * freq * 1e6 / k / 2.0
+    return distance
+
 fig, ax = plt.subplots()
 #plt.plot(distance/1e3,PC-max(PC), '--', distance/1e3,PC_intermod-max(PC), 'k-', distance/1e3, PC_intermod_leakage-max(PC), 'r.-')
 ax.plot(fftshift(distance), fftshift(PC-max(PC)), 'b', fftshift(distance), fftshift(PC_intermod-max(PC)), 'k', fftshift(distance), fftshift(PC_intermod_leakage-max(PC)), 'r')
 plt.xlabel('Distance [m]')
-secax = ax.secondary_xaxis('top', functions=(fn.distance2freq, fn.freq2distance))
+secax = ax.secondary_xaxis('top', functions=(distance2freq, freq2distance))
 secax.set_xlabel('Frequency [MHz]')
 
 
