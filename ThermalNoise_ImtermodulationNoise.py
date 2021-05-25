633786c7f738ce2472fe2accac9434f4519cd4fc
xjchen2008 <xjchen2008@gmail.com>
2021-05-25 15:24:50 -0400

    1. Similar to measurement at -50MHz. This is due to the antenna leakage set up to 1e0 amplitude which is a lot higher than target.

--- ThermalNoise_ImtermodulationNoise.py
+++ ThermalNoise_ImtermodulationNoise.py
@@ -6,16 +6,18 @@ import functions as fn
 from scipy import signal
 
 
-N = 499999
+N = 4999
 win= 1 #np.blackman(N)
 coe = fn.Coe(fc=50e6, bw=20e6, fs=250e6, N=N) # Call the class. play with the params here!
 freq= coe.freq
 distance = coe.distance
 x_cx = coe.y_cx
-x = coe.y_cx.real
+#x_cx = np.around(np.power(2,15) * x_cx) # simulate the quantization noise in FPGA fixed point
+
+x = coe.y_cx.real #+ coe.y_noise # received signal template
+tx = x_cx #+ coe.y_noise # template signal for transmit
 #x_cx = signal.hilbert(coe.y_cx.real, axis = 0)
 #x_cx = x_cx * coe.y_cx_sine2
-#x_cx = np.around(np.power(2,15) * x_cx) # simulate the quantization noise in FPGA fixed point
 
 ####################################################
 d1 = 10 #9 # What happened to pulse compression if change the delay from 10 to 9. The phase discontinuity will smear the energy to other frequency. https://www.keysight.com/us/en/assets/7018-06760/application-notes/5952-8898.pdf?success=true
@@ -24,7 +26,7 @@ d3 = 300
 y_delay_1 = np.concatenate((x[N-d1-1:-1], x[:N-d1]), axis=0) # What happened to pulse compression if change the delay from 10 to 9. The phase discontinuity will smear the energy to other frequency.
 y_delay_2 = np.concatenate((x[N-d2-1:-1], x[:N-d2]), axis=0)
 y_delay_3 = np.concatenate((x[N-d3-1:-1], x[:N-d3]), axis=0)
-vin1 = 1e-4*y_delay_1 + 1e-7*y_delay_2 + 1e-5*y_delay_3 # With leakage, no intermodulatoin #1*y_cx_delay_1 + 1e-7*y_cx_delay_2 + 1e-5*y_cx_delay_3
+vin1 = 1e0*y_delay_1 + 1e-7*y_delay_2 + 1e-5*y_delay_3 # With leakage, no intermodulatoin #1*y_cx_delay_1 + 1e-7*y_cx_delay_2 + 1e-5*y_cx_delay_3
 vin2 = 1e-7*y_delay_2 + 1e-5*y_delay_3 # No leakage signal
 vin2 = vin2 + 0.001*np.power(vin2,2)- 0.001*np.power(vin2,3) # Intermodulation of vin2
 vin3 = vin1 + 0.001*np.power(vin1,2)- 0.001*np.power(vin1,3) # + 0.001*np.power(vin1,5) + 0.001*np.power(vin1,7)# Intermodulation of vin1 with leakage
@@ -42,7 +44,7 @@ plt.plot(fftshift(freq)/1e6, fftshift(V0-max(V0)))
 plt.grid()
 plt.title('Original Signal')
 plt.figure()
-plt.plot(fftshift(freq)/1e6, fftshift(V1-max(V1)),'b*', fftshift(freq)/1e6, fftshift(V2-max(V2)),'k', fftshift(freq)/1e6, fftshift(V3-max(V3)),'r')
+plt.plot(fftshift(freq)/1e6, fftshift(V1-max(V1)),'b*-', fftshift(freq)/1e6, fftshift(V2-max(V2)),'k', fftshift(freq)/1e6, fftshift(V3-max(V3)),'r')
 #plt.plot(freq/1e6, vin1,'b', freq/1e6, vin2,'k', freq/1e6, vin3,'r')
 plt.title(' Intermodulated Chirp in Frequency Domain')
 plt.xlabel('Frequency [MHz]')
@@ -56,9 +58,9 @@ plt.legend(['No Intermodulation with leakge signal',
 #####################################
 
 
-PC = fn.PulseCompr(vin1,x_cx,np.blackman(N))
-PC_intermod = fn.PulseCompr(vin2,x_cx,np.blackman(N))
-PC_intermod_leakage = fn.PulseCompr(vin3,x_cx,np.blackman(N))
+PC = fn.PulseCompr(rx=vin1,tx=tx,win=np.blackman(N))
+PC_intermod = fn.PulseCompr(rx=vin2,tx=tx,win=np.blackman(N))
+PC_intermod_leakage = fn.PulseCompr(rx=vin3,tx=tx,win=np.blackman(N))
 #f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 #ax1.plot(np.fft.fftshift(PC))
 #ax1.set_xlabel('Number of points')
@@ -88,7 +90,7 @@ def freq2distance(freq):
 
 fig, ax = plt.subplots()
 #plt.plot(distance/1e3,PC-max(PC), '--', distance/1e3,PC_intermod-max(PC), 'k-', distance/1e3, PC_intermod_leakage-max(PC), 'r.-')
-ax.plot(fftshift(distance), fftshift(PC-max(PC)), 'b', fftshift(distance), fftshift(PC_intermod-max(PC)), 'k', fftshift(distance), fftshift(PC_intermod_leakage-max(PC)), 'r')
+ax.plot(fftshift(distance), fftshift(PC-max(PC)), 'b*-', fftshift(distance), fftshift(PC_intermod-max(PC)), 'k', fftshift(distance), fftshift(PC_intermod_leakage-max(PC)), 'r')
 plt.xlabel('Distance [m]')
 secax = ax.secondary_xaxis('top', functions=(distance2freq, freq2distance))
 secax.set_xlabel('Frequency [MHz]')
